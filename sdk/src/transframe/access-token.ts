a6d3323642b136f57a2e72aca91a4dac317e9178
Austin <austinhallock@gmail.com>
2024-05-02 14:58:18 -1000

    empty accesstoken & logs if error fetching

--- sdk/src/transframe/access-token.ts
+++ sdk/src/transframe/access-token.ts
@@ -1,6 +1,11 @@
 import { getEmbedConsumer } from './orchestrator-to-anyweb-unprivileged-consumer';
 
 export async function getAccessToken(onChange?: (accessToken: string) => void): Promise<string> {
-  const accessToken = (await getEmbedConsumer().call('userGetAccessToken', onChange)) as string;
+  const accessToken = getEmbedConsumer().call('userGetAccessToken', onChange)
+    .then((accessToken) => accessToken as string)
+    .catch(() => {
+      console.error('Failed to get access token, is the embed in a Truffle frame?');
+      return '';
+    });
   return accessToken;
 }
